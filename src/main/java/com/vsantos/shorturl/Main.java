5c8172b29079cd363f594ed845a29192c43fb2f6
vsantos1711 <vsantos067100@gmail.com>
2024-11-19 22:47:44 -0300

    Implement AWS Lambda request handler to generate short URLs

--- src/main/java/com/vsantos/shorturl/Main.java
+++ src/main/java/com/vsantos/shorturl/Main.java
@@ -1,7 +1,40 @@
 package com.vsantos.shorturl;
 
-public class Main {
-    public String handleRequest() {
-        return "Hello World!";
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = input.get("body").toString();
+
+        Map<String, String> bodyMap;
+
+        try {
+            bodyMap = mapper.readValue(body, Map.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing request body", e);
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("shortUrl", "https://shorturl.com/" + shortUrlCode);
+        response.put("originalUrl", originalUrl);
+        response.put("expirationDate", expirationTime);
+
+        return response;
     }
+
 }
